bb68d6b22fa7fb99a79905a9464d0198b05c7633
olga <diakonovao@gmail.com>
2020-07-07 23:33:51 -0400

    Added GET request function to UserController to show user's full name

--- src/main/java/com/go2it/springboot/controller/UserController.java
+++ src/main/java/com/go2it/springboot/controller/UserController.java
@@ -3,6 +3,7 @@ package com.go2it.springboot.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.go2it.springboot.entity.Role;
 import com.go2it.springboot.entity.User;
+import com.go2it.springboot.entity.dto.UserDTO;
 import com.go2it.springboot.service.IRoleService;
 import com.go2it.springboot.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
@@ -38,5 +40,25 @@ public class UserController {
             return new ResponseEntity(HttpStatus.OK);
         }
         return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public String showUserLogin(@PathVariable String id) {
+        UserDTO userDto = new UserDTO();
+        String userFullName = "";
+        userService.findById(Integer.valueOf(id)).ifPresent(user -> {
+            userDto.setFirst_name(user.getFirst_name());
+            userDto.setLast_name(user.getLast_name());
+        });
+
+        if(userDto.getFirst_name() == null && userDto.getLast_name() == null){
+            userFullName = "Unknown User";
+        }else {
+            userFullName = userDto.getFirst_name() + " " + userDto.getLast_name();
+        }
+        String htmlText = "<div style=\"text-align:center;\">" + "<h1>You are logged in as " + userFullName + "</h1>" + "</div>";
+
+        return htmlText;
+
     }
 }
